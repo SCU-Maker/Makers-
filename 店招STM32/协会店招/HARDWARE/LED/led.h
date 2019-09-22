9520c56800cf423cad4bc04da07a45d25fe778a5
SCU-Maker <Blank_Tt@163.com>
2019-09-22 11:15:53 +0800

    店招

---
+++ 店招STM32/协会店招/HARDWARE/LED/led.h
@@ -0,0 +1,31 @@
+#ifndef __LED_H
+#define __LED_H	 
+#include "sys.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+			  
+#define LED0 PAout(0)// PB5
+#define LED1 PAout(1)// PE5	
+#define LED2 PAout(2)// 1
+#define LED3 PAout(3)// PB5
+#define LED4 PAout(4)// PB5
+#define LED5 PAout(5)// 2
+#define LED6 PAout(6)// PE5	
+#define LED7 PAout(7)// PB5
+#define LED8 PAout(8)// 3
+#define LED9 PAout(11)// PB5
+#define LED10 PBout(14)// PB5
+#define LED11 PBout(15)// 4	
+#define LED12 PBout(0)// PB5
+#define LED13 PBout(1)// PB5
+#define LED14 PBout(6)// 5
+#define LED15 PBout(13)// PB5
+#define LED16 PBout(7)// PE5	
+#define LED17 PBout(8)// 6
+#define LED18 PBout(9)// PB5
+#define LED19 PBout(10)// PB5
+#define LED20 PBout(11)// 7
+
+void LED_Init(void);//≥ı ºªØ
+
+		 				    
+#endif
